5a0499e7164306e79ff95c2e173b01e89dea94ac
Pratik <pratik008@gmail.com>
2020-02-07 12:05:58 -0800

    streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,15 @@ import streamlit as st
 
 st.title("Drug-Drug Interaction App")
 
-drug1 = st.selectbox(label='Drug1', options=['Asperin', 'Advil', 'Tylenol'])
+drug1 = st.selectbox(label='Drug2', options=['Asperin', 'Advil', 'Tylenol'])
 
-drug2 = st.selectbox(label='Drug2', options=['Asperin', 'Advil', 'Tylenol'])
+drug2 = st.selectbox(label='Drug1', options=['Asperin', 'Acetomophin', 'Random', 'Unknown'])
+st.write("My App Part 2")
 
 st.write("%s+%s=%s"% (drug1,drug2,"Problem unknown"))
 
+
+
 import tensorflow
 tensorflow.__version__
+
